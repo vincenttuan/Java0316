5129afbebdd8544c15ea305fccc7be9309773d1e
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2021-03-16 20:32:33 +0800

    Update HelloWorld.java

--- src/main/java/com/ocp/day01/HelloWorld.java
+++ src/main/java/com/ocp/day01/HelloWorld.java
@@ -3,5 +3,6 @@ package com.ocp.day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Java 的第一堂課 !");
+        System.out.println("Hello Java 的第一堂課 !");
     }
 }
