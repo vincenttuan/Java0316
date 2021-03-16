c74f72e57199b93a83ed07665a1a17c3fdbe2a29
MB-teacher <MB-teacher@192.168.1.157>
2021-03-16 20:57:12 +0800

    Java

---
+++ src/main/java/com/ocp/day01/Bank.java
@@ -0,0 +1,16 @@
+package com.ocp.day01;
+
+public class Bank {
+    public static void main(String[] args) {
+        Account a01 = new Account(); // 建立 a01 帳戶
+        Account a02 = new Account(); // 建立 a02 帳戶
+        Account a03 = new Account(); // 建立 a03 帳戶
+        a01.balance = 1000;
+        a02.balance = 2000;
+        a03.balance = 3000;
+        System.out.printf("a01 帳戶餘額: $%,d\n", a01.balance);
+        System.out.printf("a02 帳戶餘額: $%,d\n", a02.balance);
+        System.out.printf("a03 帳戶餘額: $%,d\n", a03.balance);
+        
+    }
+}
