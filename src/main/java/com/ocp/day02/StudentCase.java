3ab81d9ac4d6656dea6e1dfe6302d345949acd8c
MB-teacher <MB-teacher@192.168.1.157>
2021-03-18 21:01:27 +0800

    Java

---
+++ src/main/java/com/ocp/day02/StudentCase.java
@@ -0,0 +1,8 @@
+package com.ocp.day02;
+
+public class StudentCase {
+    public static void main(String[] args) {
+        Student s1 = new Student("小明", 18, 170, 60);
+        s1.print();
+    }
+}
