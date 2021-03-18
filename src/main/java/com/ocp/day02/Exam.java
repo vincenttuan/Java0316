5b993afe812ce23af314b30d221ff55e7dba37b0
MB-teacher <MB-teacher@192.168.1.157>
2021-03-18 20:34:15 +0800

    Java

---
+++ src/main/java/com/ocp/day02/Exam.java
@@ -0,0 +1,17 @@
+package com.ocp.day02;
+
+public class Exam {
+    String name;
+    int chinese;
+    int math;
+    int getSum() {
+        return chinese + math;
+    }
+    double getAvg() {
+        return getSum() / 2.0;
+    }
+    String getPrintData() {
+        return String.format("姓名:%s 國文:%d 數學:%d 總分:%d 平均:%.1f\n", 
+                name, chinese, math, getSum(), getAvg());
+    }
+}
