a734a6bac7f6564f45df985b346906db484350d3
MB-teacher <MB-teacher@192.168.1.157>
2021-03-18 20:58:56 +0800

    Java

--- src/main/java/com/ocp/day02/Student.java
+++ src/main/java/com/ocp/day02/Student.java
@@ -3,13 +3,16 @@ package com.ocp.day02;
 public class Student {
     String name;
     int age;
-
-    Student(String name, int age) {
+    BMI bmi;
+    // 設計建構子
+    Student(String name, int age, double height, double weight) {
         this.name = name;
         this.age = age;
+        this.bmi = new BMI(name, height, weight);
     }
     
     void print() {
         System.out.printf("姓名:%s 年齡:%d\n", name, age);
+        bmi.print();
     }
 }
