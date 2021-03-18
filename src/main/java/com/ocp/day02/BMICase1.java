7f8c05805e31ed2593f4f275a979f28dd98ab9c8
MB-teacher <MB-teacher@192.168.1.157>
2021-03-18 18:59:51 +0800

    Java

---
+++ src/main/java/com/ocp/day02/BMICase1.java
@@ -0,0 +1,12 @@
+package com.ocp.day02;
+
+public class BMICase1 {
+    public static void main(String[] args) {
+        String name1 = "小明";
+        double height1 = 170;
+        double weight1 = 60;
+        double bmi1 = weight1 / Math.pow(height1/100, 2);
+        System.out.printf("姓名:%s 身高:%.1f 體重:%.1f BMI:%.2f\n", name1, height1, weight1, bmi1);
+        
+    }
+}
