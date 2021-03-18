fc82a12c698057809f918a495095c5ccc61e2bb7
MB-teacher <MB-teacher@192.168.1.157>
2021-03-18 21:23:17 +0800

    Java

--- src/main/java/com/ocp/day02/Student.java
+++ src/main/java/com/ocp/day02/Student.java
@@ -4,6 +4,7 @@ public class Student {
     String name;
     int age;
     BMI bmi;
+    Exam exam;
     // 設計建構子
     Student(String name, int age, double height, double weight) {
         this.name = name;
@@ -14,5 +15,10 @@ public class Student {
     void print() {
         System.out.printf("姓名:%s 年齡:%d\n", name, age);
         bmi.print();
+        if(exam == null) {
+            System.out.println("此學生無成績");
+        } else {
+            System.out.println(exam.getPrintData());
+        }
     }
 }
--- src/main/java/com/ocp/day02/StudentCase.java
+++ src/main/java/com/ocp/day02/StudentCase.java
@@ -4,5 +4,8 @@ public class StudentCase {
     public static void main(String[] args) {
         Student s1 = new Student("小明", 18, 170, 60);
         s1.print();
+        // 學生加入考試成績物件
+        s1.exam = new Exam(s1.name, 100, 90);
+        s1.print();
     }
 }
