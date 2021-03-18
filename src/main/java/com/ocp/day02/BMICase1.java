8724f9a65b98793e3c9ba69f2af95774629a6bba
MB-teacher <MB-teacher@192.168.1.157>
2021-03-18 19:02:02 +0800

    Java

--- src/main/java/com/ocp/day02/BMICase1.java
+++ src/main/java/com/ocp/day02/BMICase1.java
@@ -6,7 +6,8 @@ public class BMICase1 {
         double height1 = 170;
         double weight1 = 60;
         double bmi1 = weight1 / Math.pow(height1/100, 2);
-        System.out.printf("姓名:%s 身高:%.1f 體重:%.1f BMI:%.2f\n", name1, height1, weight1, bmi1);
+        String result1 = (bmi1 > 23) ? "過重" : (bmi1 < 18) ? "過輕" : "正常";
+        System.out.printf("姓名:%s 身高:%.1f 體重:%.1f BMI:%.2f 結果:%s\n", name1, height1, weight1, bmi1, result1);
         
     }
 }
