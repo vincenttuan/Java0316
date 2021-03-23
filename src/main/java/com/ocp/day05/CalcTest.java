9c50a0df331a0f8fe3ddaf4f0d18c0045275255f
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 20:00:00 +0800

    Java

--- src/main/java/com/ocp/day05/CalcTest.java
+++ src/main/java/com/ocp/day05/CalcTest.java
@@ -1,11 +1,20 @@
 package com.ocp.day05;
 import com.ocp.day05.util.Calc;
+//import static com.ocp.day05.util.Calc.*;
+import static com.ocp.day05.util.Calc.add;
+import static com.ocp.day05.util.Calc.sub;
+import static com.ocp.day05.util.Calc.mul;
+import static com.ocp.day05.util.Calc.div;
 public class CalcTest {
     public static void main(String[] args) {
         System.out.println(Calc.add(20, 10));
         System.out.println(Calc.sub(20, 10));
         System.out.println(Calc.mul(20, 10));
         System.out.println(Calc.div(20, 10));
-        
+        // 省去撰寫 Calc 的麻煩
+        System.out.println(add(20, 10));
+        System.out.println(sub(20, 10));
+        System.out.println(mul(20, 10));
+        System.out.println(div(20, 10));
     }
 }
