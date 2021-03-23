2ba6bf33f5c619f6f1b2037d25fd7ed9ff03cb3d
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 19:26:06 +0800

    Java

--- src/main/java/com/ocp/day03/AccessDemo.java
+++ src/main/java/com/ocp/day03/AccessDemo.java
@@ -3,6 +3,7 @@ package com.ocp.day03;
 import com.ocp.day03.bar.John;
 import com.ocp.day03.bar.Mary;
 import com.ocp.day03.foo.Bob;
+import com.ocp.day03.foo.Helen;
 
 public class AccessDemo {
     public static void main(String[] args) {
@@ -12,5 +13,7 @@ public class AccessDemo {
         mary.print();
         Bob bob = new Bob();
         bob.print();
+        Helen helen = new Helen();
+        helen.print();
     }
 }
