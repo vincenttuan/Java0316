12a23428c645f8e342b613bbf812e2f54ec5c95e
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 19:24:16 +0800

    Java

--- src/main/java/com/ocp/day03/foo/Helen.java
+++ src/main/java/com/ocp/day03/foo/Helen.java
@@ -1,5 +1,11 @@
 package com.ocp.day03.foo;
 
-public class Helen {
-    
+import com.ocp.day03.bar.John;
+
+public class Helen extends John {
+    public void print() {
+        System.out.print("Helen: ");
+        System.out.print(a + ", ");
+        System.out.print(b + "\n");
+    }
 }
