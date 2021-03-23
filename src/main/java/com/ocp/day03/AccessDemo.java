20076068cfacd57c78654e856bba223ac9d7e960
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 19:16:50 +0800

    Java

--- src/main/java/com/ocp/day03/AccessDemo.java
+++ src/main/java/com/ocp/day03/AccessDemo.java
@@ -2,6 +2,7 @@ package com.ocp.day03;
 
 import com.ocp.day03.bar.John;
 import com.ocp.day03.bar.Mary;
+import com.ocp.day03.foo.Bob;
 
 public class AccessDemo {
     public static void main(String[] args) {
@@ -9,5 +10,7 @@ public class AccessDemo {
         john.print();
         Mary mary = new Mary();
         mary.print();
+        Bob bob = new Bob();
+        bob.print();
     }
 }
--- src/main/java/com/ocp/day03/foo/Bob.java
+++ src/main/java/com/ocp/day03/foo/Bob.java
@@ -1,5 +1,14 @@
 package com.ocp.day03.foo;
 
+import com.ocp.day03.bar.John;
+
 public class Bob {
-    
+    public void print() {
+        John john = new John();
+        System.out.print("Bob: ");
+        System.out.print(john.a + "\n");
+        //System.out.print(john.b + "\n");
+        //System.out.print(john.c + "\n");
+        //System.out.print(john.d + "\n");
+    }
 }
