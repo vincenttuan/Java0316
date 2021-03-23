326ae8e988aa9b8b2fc917789d3c081112717a71
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 19:45:22 +0800

    Java

--- src/main/java/com/ocp/day04/AccessMoney.java
+++ src/main/java/com/ocp/day04/AccessMoney.java
@@ -1,7 +1,15 @@
 package com.ocp.day04;
-
+import com.ocp.day04.bar.Money;
+//import com.ocp.day04.foo.Money;
 public class AccessMoney {
     public static void main(String[] args) {
+        Money m1 = new Money();
+        System.out.println(m1.amount);
+        // 打上完整路徑
+        com.ocp.day04.foo.Money m2 = new com.ocp.day04.foo.Money();
+        System.out.println(m2.amount);
+        
+        System.out.println(m1.amount + m2.amount);
         
     }
 }
