97fff832a307449e0ad157fafbc3e7fd6909b24f
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 18:58:48 +0800

    Java

--- src/main/java/com/ocp/day03/bar/John.java
+++ src/main/java/com/ocp/day03/bar/John.java
@@ -1,5 +1,16 @@
 package com.ocp.day03.bar;
 
 public class John {
+    public int a = 100;
+    protected int b = 200;
+    int c = 300;
+    private int d = 400;
+    
+    public void print() {
+        System.out.print(a + ", ");
+        System.out.print(b + ", ");
+        System.out.print(c + ", ");
+        System.out.print(d + "\n");
+    }
     
 }
