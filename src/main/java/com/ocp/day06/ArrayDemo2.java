28bce821cc9dbe57a0c65d2a883c507f0cbc1f3f
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 21:32:02 +0800

    Java

--- src/main/java/com/ocp/day06/ArrayDemo2.java
+++ src/main/java/com/ocp/day06/ArrayDemo2.java
@@ -7,8 +7,13 @@ public class ArrayDemo2 {
         int[] scores = {100, 90, 90, 80, 70, 50, 40, 100, 75, 15};
         System.out.printf("%s, 長度: %d\n", Arrays.toString(scores), scores.length);
         // 總分?
-        
+        int sum = 0;
+        for(int i=0;i<scores.length;i++) {
+            sum += scores[i]; // += 累加
+        }
         // 平均?
+        double avg = (double)sum/scores.length;
+        System.out.printf("總分: %d 平均: %.1f\n", sum, avg);
         
     }
 }
