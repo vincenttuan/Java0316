e2de6a367bcad826c70b1279ac782380fc4949a2
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 19:50:02 +0800

    Java

---
+++ src/main/java/com/ocp/day05/util/Calc.java
@@ -0,0 +1,16 @@
+package com.ocp.day05.util;
+
+public class Calc {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static int sub(int x, int y) {
+        return x - y;
+    }
+    public static int mul(int x, int y) {
+        return x * y;
+    }
+    public static int div(int x, int y) {
+        return x / y;
+    }
+}
