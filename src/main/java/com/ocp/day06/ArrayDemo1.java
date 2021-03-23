be11e08ce1514ac35f3741e741e37bf993d31cc3
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 21:10:30 +0800

    Java

---
+++ src/main/java/com/ocp/day06/ArrayDemo1.java
@@ -0,0 +1,15 @@
+package com.ocp.day06;
+import java.util.Arrays;
+public class ArrayDemo1 {
+    public static void main(String[] args) {
+        int[] scores = new int[3];
+        scores[0] = 100;
+        scores[1] = 90;
+        scores[2] = 90;
+        int lens = scores.length; // 取得陣列長度
+        System.out.printf("陣列長度: %d\n", lens); 
+        // 印出陣列每一個元素資料
+        System.out.println("[" + scores[0] + ", " + scores[1] + ", " + scores[2] + "]");
+        System.out.println(Arrays.toString(scores));
+    }
+}
