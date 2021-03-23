440da9461b6dcdb08f5eefdf563834f22cbaf5de
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 21:21:01 +0800

    Java

---
+++ src/main/java/com/ocp/day06/ArrayDemo2.java
@@ -0,0 +1,11 @@
+package com.ocp.day06;
+
+import java.util.Arrays;
+
+public class ArrayDemo2 {
+    public static void main(String[] args) {
+        int[] scores = {100, 90, 90, 80, 70, 50, 40, 100, 75, 15};
+        System.out.printf("%s, 長度: %d\n", Arrays.toString(scores), scores.length);
+        
+    }
+}
