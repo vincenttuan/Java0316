73db3d35cc49f61969c5d1c32d2e7e906c81a395
MB-teacher <MB-teacher@192.168.1.157>
2021-03-23 19:53:32 +0800

    Java

---
+++ src/main/java/com/ocp/day05/CalcTest.java
@@ -0,0 +1,11 @@
+package com.ocp.day05;
+import com.ocp.day05.util.Calc;
+public class CalcTest {
+    public static void main(String[] args) {
+        System.out.println(Calc.add(20, 10));
+        System.out.println(Calc.sub(20, 10));
+        System.out.println(Calc.mul(20, 10));
+        System.out.println(Calc.div(20, 10));
+        
+    }
+}
