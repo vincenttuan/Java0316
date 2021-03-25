2b9c98d5ed6bb21976bf7585ee6f12fa219b15c7
MB-teacher <MB-teacher@192.168.1.157>
2021-03-25 20:02:19 +0800

    Java

--- src/main/java/com/ocp/day06/ArrayCV3.java
+++ src/main/java/com/ocp/day06/ArrayCV3.java
@@ -8,6 +8,11 @@ package com.ocp.day06;
 */
 public class ArrayCV3 {
     public static void main(String[] args) {
-        
+        double[] h = {172, 168, 164, 170, 176};
+        double[] w = {62, 57, 58, 64, 64};
+        double h_cv = MyMath.cv(h);
+        double w_cv = MyMath.cv(w);
+        String result = (h_cv < w_cv)?"身高":"體重";
+        System.out.printf("身高cv: %.2f%% 體種cv: %.2f%% %s集中度較高", h_cv*100, w_cv*100, result);
     }
 }
