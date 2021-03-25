9876082afda6ce4a9cd481d69dfd2682d4557561
MB-teacher <MB-teacher@192.168.1.157>
2021-03-25 20:57:16 +0800

    Java

--- src/main/java/com/ocp/day06/ArrayCV4.java
+++ src/main/java/com/ocp/day06/ArrayCV4.java
@@ -8,9 +8,15 @@ package com.ocp.day06;
 6.8 7.2 6.8 7.5 6.9 7.9 7.9 7.1 7.2
 試分別求此公司的員工投資股票與公債的獲利率變異係數
 以及哪一種投資的獲利較穩健 ?
+求出股票投資最大獲利與最低獲利的獲利率為何 ?
  */
 public class ArrayCV4 {
     public static void main(String[] args) {
+        double[] stocks = {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5};
+        double[] bonds = {6.8, 7.2, 6.8, 7.5, 6.9, 7.9, 7.9, 7.1, 7.2};
+        double stocks_cv = MyMath.cv(stocks);
+        double bonds_cv = MyMath.cv(bonds);
+        String result = stocks_cv < bonds_cv ? "股票" : "公債";
         
     }
 }
