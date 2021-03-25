1dcf70df53119aeeba64c76c3599d6df3cd4a4eb
MB-teacher <MB-teacher@192.168.1.157>
2021-03-25 19:23:57 +0800

    Java

--- src/main/java/com/ocp/day06/ArrayCV.java
+++ src/main/java/com/ocp/day06/ArrayCV.java
@@ -20,5 +20,8 @@ public class ArrayCV {
         System.out.printf("Sum of squares: %.1f\n", sumOfSquares);
         double sd = Math.sqrt((1.0/nums.length) * sumOfSquares);
         System.out.printf("S.D: %.1f\n", sd);
+        // 變異係數 CV
+        double cv = sd / avg;
+        System.out.printf("C.V: %.1f\n", cv);
     }
 }
