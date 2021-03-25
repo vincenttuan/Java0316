f54e522da5892b805312c7d58712250eccb220f7
MB-teacher <MB-teacher@192.168.1.157>
2021-03-25 19:54:50 +0800

    Java

---
+++ src/main/java/com/ocp/day06/ArrayCV3.java
@@ -0,0 +1,13 @@
+package com.ocp.day06;
+
+/*
+調查五位學生之身高及體重如下，試比較其分散程度。
+身高(h)：172、168、164、170、176(公分)
+體重(w)：62、57、58、64、64(公斤)
+請問身高及體重哪一個較集中 ?
+*/
+public class ArrayCV3 {
+    public static void main(String[] args) {
+        
+    }
+}
