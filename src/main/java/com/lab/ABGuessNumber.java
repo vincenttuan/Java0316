6991d1529f8dc6c173b97587414784c57af944c1
MB-teacher <MB-teacher@192.168.1.157>
2021-03-30 21:13:36 +0800

    Java

--- src/main/java/com/lab/ABGuessNumber.java
+++ src/main/java/com/lab/ABGuessNumber.java
@@ -8,9 +8,29 @@ public class ABGuessNumber {
         do {            
             Scanner sc = new Scanner(System.in);
             System.out.print("請輸入一組不重複的四位數: ");
-            int guess = sc.nextInt();
-            int[] guessArray = {};
-            
+            String guess = sc.next();
+            int[] guessArray = {guess.charAt(0)-48, guess.charAt(1)-48, guess.charAt(2)-48, guess.charAt(3)-48};
+            // 判斷 a, b
+            int a = 0, b = 0;
+            for(int i=0;i<4;i++) {
+                if(ansArray[i] == guessArray[i]) {
+                    a++;
+                }
+            }
+            for(int i=0;i<4;i++) {
+                for(int k=0;k<4;k++) {
+                    if(ansArray[i] == guessArray[k]) {
+                        b++;
+                        break;
+                    }
+                }
+            }
+            b -= a; //b = b - a;
+            System.out.printf("%d A %d B\n", a, b);
+            if(a == 4) {
+                System.out.println("Bingo");
+                break;
+            }
         } while (true);
     }
 }
