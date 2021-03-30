64a496496ea6ea640e311a82bb7fed635a24b8e4
MB-teacher <MB-teacher@192.168.1.157>
2021-03-30 21:34:32 +0800

    Java

--- src/main/java/com/lab/ABGuessNumber.java
+++ src/main/java/com/lab/ABGuessNumber.java
@@ -1,10 +1,12 @@
 package com.lab;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ABGuessNumber {
     public static void main(String[] args) {
         int[] ansArray = Util.getAnsArray();//{5, 8, 4, 1};
+        System.out.println(Arrays.toString(ansArray));
         do {            
             Scanner sc = new Scanner(System.in);
             System.out.print("請輸入一組不重複的四位數: ");
