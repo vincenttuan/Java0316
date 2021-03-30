745723bece353042bc5c5fa6480fcf8ea028a92e
MB-teacher <MB-teacher@192.168.1.157>
2021-03-30 19:10:19 +0800

    Java

---
+++ src/main/java/com/ocp/day07/StringEquals3.java
@@ -0,0 +1,15 @@
+package com.ocp.day07;
+
+public class StringEquals3 {
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        System.out.println(s1 == s2);
+        s1 = s1 + "8";
+        s2 = s2 + "8";
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1 == s2);
+        System.out.println(s1.intern() == s2.intern());
+    }
+}
