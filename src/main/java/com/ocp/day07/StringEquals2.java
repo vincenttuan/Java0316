abe6e8ca273be48af824e45d2c550d59ebcd479d
MB-teacher <MB-teacher@192.168.1.157>
2021-03-30 19:05:07 +0800

    Java

--- src/main/java/com/ocp/day07/StringEquals2.java
+++ src/main/java/com/ocp/day07/StringEquals2.java
@@ -9,5 +9,8 @@ public class StringEquals2 {
         // 將 s1 放入 String pool
         s1 = s1.intern();
         System.out.println(s1 == s2);
+        String s3 = "jAvA";
+        System.out.println(s2.equals(s3)); // 有分大小寫的比較
+        System.out.println(s2.equalsIgnoreCase(s3)); // 不分大小寫的比較
     }
 }
