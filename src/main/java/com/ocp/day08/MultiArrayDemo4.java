9fd55c852934c2d32651a97ff3622a60e5f02b99
MB-teacher <MB-teacher@192.168.1.157>
2021-04-01 21:38:49 +0800

    Java

--- src/main/java/com/ocp/day08/MultiArrayDemo4.java
+++ src/main/java/com/ocp/day08/MultiArrayDemo4.java
@@ -4,6 +4,6 @@ public class MultiArrayDemo4 {
     public static void main(String[] args) {
         // 3人的身高與體重如下:
         double[][] hw = {{170, 160, 180},{60, 45, 90}};
-        // 求3人的BMI
+        // 求3人的BMI (Homework)
     }
 }
