d03477e5f8bda31d2b113c85a667db689728b517
MB-teacher <MB-teacher@192.168.1.157>
2021-04-01 21:38:35 +0800

    Java

---
+++ src/main/java/com/ocp/day08/MultiArrayDemo4.java
@@ -0,0 +1,9 @@
+package com.ocp.day08;
+
+public class MultiArrayDemo4 {
+    public static void main(String[] args) {
+        // 3人的身高與體重如下:
+        double[][] hw = {{170, 160, 180},{60, 45, 90}};
+        // 求3人的BMI
+    }
+}
