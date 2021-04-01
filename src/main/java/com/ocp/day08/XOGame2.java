6831f869ae685b73bf1a0a8a88387b1c4fdd5bbb
MB-teacher <MB-teacher@192.168.1.157>
2021-04-01 21:11:10 +0800

    Java

--- src/main/java/com/ocp/day08/XOGame2.java
+++ src/main/java/com/ocp/day08/XOGame2.java
@@ -1,5 +1,6 @@
 package com.ocp.day08;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class XOGame2 {
@@ -8,9 +9,19 @@ public class XOGame2 {
         do {            
             Scanner sc = new Scanner(System.in);
             print(xo);
+            // user
             System.out.print("請選擇(0~8) ==> ");
             int n = sc.nextInt();
             xo[n/3][n%3] = 'O';
+            // pc
+            while (true) {                
+                int m = new Random().nextInt(9); // 0~8
+                if(xo[m/3][m%3] != ' ') {
+                    continue;
+                }
+                xo[m/3][m%3] = 'X';
+                break;
+            }
             
         } while (true);
         
