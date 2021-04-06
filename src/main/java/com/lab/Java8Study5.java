c978f895271c2331682eb68fe5b41bc3616da8ba
MB-teacher <MB-teacher@192.168.1.157>
2021-04-06 20:48:39 +0800

    Java

---
+++ src/main/java/com/lab/Java8Study5.java
@@ -0,0 +1,10 @@
+package com.lab;
+
+public class Java8Study5 {
+    public static void main(String[] args) {
+        String[] scores = {"80", "50", "70", "90", "40"};
+        // 請算出及格成績的總和
+        // java 7
+        
+    }
+}
