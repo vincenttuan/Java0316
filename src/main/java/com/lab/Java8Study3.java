fa51a30f2b686b2e9ee0095a8e4ad3da3d2962f5
MB-teacher <MB-teacher@192.168.1.157>
2021-04-06 20:06:54 +0800

    Java

--- src/main/java/com/lab/Java8Study3.java
+++ src/main/java/com/lab/Java8Study3.java
@@ -1,8 +1,24 @@
 package com.lab;
 
+import java.util.stream.IntStream;
+
 public class Java8Study3 {
     public static void main(String[] args) {
         int[] scores = {80, 50, 70, 90, 40};
-        
+        // 算出總分與平均 ?
+        // java 7
+        int sum1 = 0;
+        for(int s : scores) {
+            sum1 += s;
+        }
+        double avg1 = (double)sum1 / scores.length;
+        System.out.println(sum1);
+        System.out.println(avg1);
+        // java 8
+        int sum2 = IntStream.of(scores).sum();
+        double avg2 = IntStream.of(scores).average().getAsDouble();
+        //double avg3 = (double)sum2 / scores.length;
+        System.out.println(sum2);
+        System.out.println(avg2);
     }
 }
