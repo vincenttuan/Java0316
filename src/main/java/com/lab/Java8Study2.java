2d7ccefa0311ff2c3c21abaf2024710d7e23abc6
MB-teacher <MB-teacher@192.168.1.157>
2021-04-06 19:55:19 +0800

    Java

--- src/main/java/com/lab/Java8Study2.java
+++ src/main/java/com/lab/Java8Study2.java
@@ -1,14 +1,20 @@
 package com.lab;
 
+import java.util.stream.IntStream;
+
 public class Java8Study2 {
     public static void main(String[] args) {
         int[] scores = {80, 50, 70, 90, 40};
         // 列印及格的分數
         // java 7
-        for(int s : scores) {
-            if(s >= 60) {
+        for(int s : scores) { // foreach
+            if(s >= 60) { // 過濾條件 filter
                 System.out.println(s);
             }
         }
+        // java 8
+        IntStream.of(scores)
+                .filter(s -> s >= 60)
+                .forEach(s -> System.out.println(s));
     }
 }
