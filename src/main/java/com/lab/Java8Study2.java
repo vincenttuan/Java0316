5adeed8aaf095af9b7aca19832761483bc561a51
MB-teacher <MB-teacher@192.168.1.157>
2021-04-06 19:48:12 +0800

    Java

--- src/main/java/com/lab/Java8Study2.java
+++ src/main/java/com/lab/Java8Study2.java
@@ -3,6 +3,12 @@ package com.lab;
 public class Java8Study2 {
     public static void main(String[] args) {
         int[] scores = {80, 50, 70, 90, 40};
-        
+        // 列印及格的分數
+        // java 7
+        for(int s : scores) {
+            if(s >= 60) {
+                System.out.println(s);
+            }
+        }
     }
 }
