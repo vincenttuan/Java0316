2225dc01a30d29a54d478fe44566ea5a1de38741
MB-teacher <MB-teacher@192.168.1.157>
2021-04-06 21:07:07 +0800

    Java

--- src/main/java/com/lab/Java8Study5.java
+++ src/main/java/com/lab/Java8Study5.java
@@ -1,10 +1,19 @@
 package com.lab;
 
+import java.util.IntSummaryStatistics;
+import java.util.stream.Stream;
+
 public class Java8Study5 {
     public static void main(String[] args) {
         String[] scores = {"80", "50", "70", "90", "40"};
-        // 請算出及格成績的總和
-        // java 7
+        // 請算出及格成績的總和, 最高分, 最低分
+        // java 8
+        IntSummaryStatistics stat = Stream.of(scores)
+                                    .mapToInt(s -> Integer.parseInt(s)) // 得到 IntStream
+                                    .filter(s -> s >= 60) // 過濾及格分數
+                                    .summaryStatistics(); // 統計物件
+        System.out.printf("sum: %d, max: %d, min: %d\n", 
+                          stat.getSum(), stat.getMax(), stat.getMin());
         
     }
 }
