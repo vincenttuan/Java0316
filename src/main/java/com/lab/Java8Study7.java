312c649247ad6e930d6ebf3f0f198b9fc4b283ad
MB-teacher <MB-teacher@192.168.1.157>
2021-04-06 21:40:43 +0800

    Java

---
+++ src/main/java/com/lab/Java8Study7.java
@@ -0,0 +1,15 @@
+package com.lab;
+
+import java.util.stream.Stream;
+
+public class Java8Study7 {
+    public static void main(String[] args) {
+        String[][] names = {{"Jo", "Mary"}, {"John", "Helen"}, {"Bob"}};
+        // 求所有字母的總和 ?
+        int sum = Stream.of(names)
+                    .flatMap(s -> Stream.of(s)) // String[] 轉 Stream
+                    .mapToInt(s -> s.length())
+                    .sum();
+        
+    }
+}
