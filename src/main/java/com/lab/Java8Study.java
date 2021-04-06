098aa323ea12767b8d7663f0f76c829a64973f23
MB-teacher <MB-teacher@192.168.1.157>
2021-04-06 19:31:59 +0800

    Java

--- src/main/java/com/lab/Java8Study.java
+++ src/main/java/com/lab/Java8Study.java
@@ -2,6 +2,16 @@ package com.lab;
 
 public class Java8Study {
     public static void main(String[] args) {
+        int[] scores = {80, 50, 70, 90, 40};
+        // 請印出所有成績 ?
+        // java 7
+        for(int i=0;i<scores.length;i++) {
+            System.out.println(scores[i]);
+        }
+        for(int s : scores) {
+            System.out.println(s);
+        }
+        
         
     }
 }
