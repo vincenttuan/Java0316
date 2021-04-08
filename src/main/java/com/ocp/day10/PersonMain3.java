5c44130c25bc7a1e2625b16613ac8a7b62819267
MB-teacher <MB-teacher@192.168.1.157>
2021-04-08 21:03:57 +0800

    Java

--- src/main/java/com/ocp/day10/PersonMain3.java
+++ src/main/java/com/ocp/day10/PersonMain3.java
@@ -1,5 +1,9 @@
 package com.ocp.day10;
 
+import java.util.DoubleSummaryStatistics;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 public class PersonMain3 {
     public static void main(String[] args) {
         Person[] persons = {
@@ -10,6 +14,12 @@ public class PersonMain3 {
             new Person("Tom",   21, 190, 120.5),
         };
         // 計算 bmi 的平均, 最大與最小各為多少 ?
+        Function<Person, Double> getBMI = p -> p.getWeight() / Math.pow(p.getHeight()/100, 2);
+        DoubleSummaryStatistics stat = Stream.of(persons)
+                .mapToDouble(getBMI::apply) // 相當於 p -> getBMI.apply(p)
+                .summaryStatistics();
+        System.out.printf("平均: %.2f 最大: %.2f 最小: %.2f\n", 
+                stat.getAverage(), stat.getMax(), stat.getMin());
         
     }
 }
