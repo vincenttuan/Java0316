63cd66ff8300d0494d44ac1a562ac799806061ce
MB-teacher <MB-teacher@192.168.1.157>
2021-04-08 20:51:58 +0800

    Java

---
+++ src/main/java/com/ocp/day10/PersonMain3.java
@@ -0,0 +1,15 @@
+package com.ocp.day10;
+
+public class PersonMain3 {
+    public static void main(String[] args) {
+        Person[] persons = {
+            new Person("John",  17, 170, 60.5),
+            new Person("Mary",  18, 160, 50.5),
+            new Person("Jo",    19, 155, 42.5),
+            new Person("Helen", 16, 185, 75.5),
+            new Person("Tom",   21, 190, 120.5),
+        };
+        // 計算 bmi 的平均, 最大與最小各為多少 ?
+        
+    }
+}
