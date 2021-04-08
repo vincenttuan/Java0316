ebb309f8afa72050732a965ef4924d99d3c6d915
MB-teacher <MB-teacher@192.168.1.157>
2021-04-08 21:10:44 +0800

    Java

---
+++ src/main/java/com/ocp/day10/PersonMain4.java
@@ -0,0 +1,19 @@
+package com.ocp.day10;
+
+public class PersonMain4 {
+    public static void main(String[] args) {
+        Person[][] persons = {
+            { // A 班
+                new Person("John",  17, 170, 60.5),
+                new Person("Mary",  18, 160, 50.5),
+            },
+            { // B 班
+                new Person("Jo",    19, 155, 42.5),
+                new Person("Helen", 16, 185, 75.5),
+                new Person("Tom",   21, 190, 120.5),
+            },
+        };
+        // 請印出所有人的 bmi 資料
+        
+    }
+}
