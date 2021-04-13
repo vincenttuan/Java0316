2305fe2d325b4494718539694d06bac5d2b26d37
MB-teacher <MB-teacher@192.168.1.157>
2021-04-13 20:57:16 +0800

    Java

--- src/main/java/com/ocp/day11/EmployeeApi.java
+++ src/main/java/com/ocp/day11/EmployeeApi.java
@@ -1,7 +1,7 @@
 package com.ocp.day11;
 
 public class EmployeeApi {
-    private static Employee[] employees;
+    private static Employee[] employees = null;
     static {
         Employee e1 = new Employee();
         e1.setSalary(3_0000);
