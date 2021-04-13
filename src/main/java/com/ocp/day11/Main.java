c6c3821cae05aa71e1f9d7e94934a0e0e491a503
MB-teacher <MB-teacher@192.168.1.157>
2021-04-13 21:38:13 +0800

    Java

--- src/main/java/com/ocp/day11/Main.java
+++ src/main/java/com/ocp/day11/Main.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        
+        menu();
     }
     public static void menu() {
         System.out.println("----------------------");
