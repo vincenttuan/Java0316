b170fefb8e5d8cc4d3a6ba88780a3ed425f5c78d
MB-teacher <MB-teacher@192.168.1.157>
2021-04-15 21:13:44 +0800

    Java

--- src/main/java/com/ocp/day12/OverloadingDemo2.java
+++ src/main/java/com/ocp/day12/OverloadingDemo2.java
@@ -4,20 +4,18 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class OverloadingDemo2 {
-    public void calc(int x, int y) {
-        System.out.println(x + y);
-    }
-    public void calc(int x, int y, int z) {
-        System.out.println(x + y + z);
-    }
-    public void calc(int[] array) {
+    public void calc(int... array) {
         System.out.println(IntStream.of(array).sum());
     }
     
     public static void main(String[] args) {
         OverloadingDemo2 o = new OverloadingDemo2();
+        o.calc();
+        o.calc(10);
         o.calc(10, 20);
         o.calc(10, 20, 30);
+        o.calc(10, 20, 30, 40);
+        o.calc(10, 20, 30, 40, 50);
         int[] array = {10, 20, 30, 40, 50, 60};
         o.calc(array);
     }
