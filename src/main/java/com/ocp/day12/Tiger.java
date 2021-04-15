9326e891488de653c0ac34641deab4adbad2b508
MB-teacher <MB-teacher@192.168.1.157>
2021-04-15 19:13:26 +0800

    Java

--- src/main/java/com/ocp/day12/Tiger.java
+++ src/main/java/com/ocp/day12/Tiger.java
@@ -1,5 +1,10 @@
 package com.ocp.day12;
 
 public class Tiger extends Cat {
+
+    @Override
+    public void shout() {
+        System.out.println("吼");
+    }
     
 }
--- src/main/java/com/ocp/day12/Zoo.java
+++ src/main/java/com/ocp/day12/Zoo.java
@@ -2,6 +2,9 @@ package com.ocp.day12;
 
 public class Zoo {
     public static void main(String[] args) {
-        
+        Cat cat = new Cat();
+        cat.shout();
+        Tiger tiger = new Tiger();
+        tiger.shout();
     }
 }
