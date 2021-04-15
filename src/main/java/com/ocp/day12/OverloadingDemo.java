8cdbd39e5fe7141be89c3222e4e638f0fdd15ba1
MB-teacher <MB-teacher@192.168.1.157>
2021-04-15 20:47:38 +0800

    Java

---
+++ src/main/java/com/ocp/day12/OverloadingDemo.java
@@ -0,0 +1,23 @@
+package com.ocp.day12;
+
+// 超載/覆載
+public class OverloadingDemo {
+    public void calc(int x) {
+        System.out.println("int: " + x);
+    }
+    public void calc(Integer x) {
+        System.out.println("Integer: " + x);
+    }
+    public void calc(float x) {
+        System.out.println("float: " + x);
+    }
+    public void calc(double x) {
+        System.out.println("double: " + x);
+    }
+    
+    public static void main(String[] args) {
+        OverloadingDemo o = new OverloadingDemo();
+        o.calc(100);
+    }
+    
+}
