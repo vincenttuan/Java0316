3d6a67bb90f4880054dcc6a03696c8b70210b306
MB-teacher <MB-teacher@192.168.1.157>
2021-04-15 19:49:09 +0800

    Java

--- src/main/java/com/ocp/day12/Zoo3.java
+++ src/main/java/com/ocp/day12/Zoo3.java
@@ -3,6 +3,13 @@ package com.ocp.day12;
 public class Zoo3 {
     public static void main(String[] args) {
         Cat cat = new Tiger();
-        
+        cat.shout();
+        ((Tiger)cat).hunter();
+        Cat cat2 = new Cat();
+        if(cat2 instanceof Tiger) {
+            ((Tiger)cat2).hunter();
+        } else {
+            System.out.println("cat2 不可轉 Tiger");
+        }
     }
 }
