92cfe555fb0c6cf77d7729dd9e3f247cc0904b08
MB-teacher <MB-teacher@192.168.1.157>
2021-04-20 19:56:37 +0800

    Java

--- src/main/java/com/ocp/day14/Employee.java
+++ src/main/java/com/ocp/day14/Employee.java
@@ -3,6 +3,13 @@ package com.ocp.day14;
 public abstract class Employee {
     private int salary;
 
+    public Employee() {
+    }
+
+    public Employee(int salary) {
+        this.salary = salary;
+    }
+    
     public int getSalary() {
         return salary;
     }
