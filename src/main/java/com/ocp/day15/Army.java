3be0bb51f75108224a10cf5ebac3389f3e544b14
MB-teacher <MB-teacher@192.168.1.157>
2021-04-20 21:27:44 +0800

    Java

---
+++ src/main/java/com/ocp/day15/Army.java
@@ -0,0 +1,17 @@
+package com.ocp.day15;
+
+import java.util.stream.Stream;
+
+public class Army {
+    public static void main(String[] args) {
+        Fighter fighter = new Fighter();
+        Tank tank = new Tank();
+        Truck truck = new Truck();
+        
+        Car[] cars = {tank, truck};
+        Weapon[] weapons = {fighter, tank};
+        
+        Stream.of(cars).forEach(c -> c.move());
+        Stream.of(weapons).forEach(w -> w.shoot());
+    }
+}
