ea37c9cfd788b88891e6ba7c3011be5f3b62cc53
MB-teacher <MB-teacher@192.168.1.157>
2021-04-22 19:45:07 +0800

    Java

--- src/main/java/com/ocp/day16/SevenEleven.java
+++ src/main/java/com/ocp/day16/SevenEleven.java
@@ -3,11 +3,12 @@ package com.ocp.day16;
 public class SevenEleven {
     // 結帳方法
     public void submit(int price) {
+        String name = "微粒炸醬麵";
         // 交易類別
         class Transaction {
             // 交易資料
             void print() {
-                System.out.printf("交易金額: %d\n", price);
+                System.out.printf("%s 交易金額: %d\n", name, price);
             }
         }
         Transaction t = new Transaction();
