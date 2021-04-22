d406ba82f3d6e308c5ee8e8dea57d8a3d48a42a2
MB-teacher <MB-teacher@192.168.1.157>
2021-04-22 21:35:39 +0800

    Java

---
+++ src/main/java/com/ocp/day16/EvenNumber.java
@@ -0,0 +1,5 @@
+package com.ocp.day16;
+
+public interface EvenNumber { // 偶數
+    boolean isEven(int n); // n 是否是偶數
+}
---
+++ src/main/java/com/ocp/day16/PrimeNumber.java
@@ -0,0 +1,5 @@
+package com.ocp.day16;
+
+public interface PrimeNumber { // 質數
+    boolean isPrime(int n); // n 是否是質數 ?
+}
