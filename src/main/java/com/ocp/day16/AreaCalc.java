a5beed8e71c224b3625c4b62abb95852f1449cb0
MB-teacher <MB-teacher@192.168.1.157>
2021-04-22 20:01:01 +0800

    Java

--- src/main/java/com/ocp/day16/AreaCalc.java
+++ src/main/java/com/ocp/day16/AreaCalc.java
@@ -5,5 +5,13 @@ public class AreaCalc {
         double r = 15.0;
         CircleArea ca = new CicleAreaImpl();
         System.out.println(ca.getArea(r));
+        // 匿名內部類別
+        CircleArea ca2 = new CircleArea(){
+            @Override
+            public double getArea(double r) {
+                return r * r * Math.PI;
+            }
+        };
+        System.out.println(ca2.getArea(r));
     }
 }
