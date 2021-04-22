2fa57e11e3eac0a27e7d7ec7c223fa41d0a466a0
MB-teacher <MB-teacher@192.168.1.157>
2021-04-22 21:39:57 +0800

    Java

--- src/main/java/com/ocp/day16/NumberMain.java
+++ src/main/java/com/ocp/day16/NumberMain.java
@@ -3,7 +3,8 @@ package com.ocp.day16;
 public class NumberMain {
     public static void main(String[] args) {
         // Lambda 實作 EvenNumber, n = 50 是否是偶數
-        
+        EvenNumber even = (n) -> n % 2 == 0;
+        System.out.println(even.isEven(50));
         // Lambda 實作 PrimeNumber, n = 9973 是否是質數
         
     }
