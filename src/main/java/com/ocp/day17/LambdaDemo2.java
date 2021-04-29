0a5fed78bd98d6ce8e76760df55c60b658f05bd7
MB-teacher <MB-teacher@192.168.1.157>
2021-04-29 20:06:14 +0800

    Java

--- src/main/java/com/ocp/day17/LambdaDemo2.java
+++ src/main/java/com/ocp/day17/LambdaDemo2.java
@@ -1,11 +1,18 @@
 package com.ocp.day17;
 
+import java.util.DoubleSummaryStatistics;
 import java.util.stream.Stream;
 
 public class LambdaDemo2 {
     public static void main(String[] args) {
         // 求此人總分與平均 = ?
-        Stream.of("國文", 100.0, "英文", 90.5, "數學", 70.5, "座號", 7);
+        DoubleSummaryStatistics stat = Stream.of("國文", 100.0, "英文", 90.5, "數學", 70.5, "座號", 7)
+                .filter(x -> x instanceof Double)
+                .mapToDouble(x -> (double)x) // 物件 Double 轉基本 double
+                .summaryStatistics();
+        
+        System.out.println(stat.getSum());
+        System.out.println(stat.getAverage());
         
     }
 }
