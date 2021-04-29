37357f838098a136025a1431071411b4fb877285
MB-teacher <MB-teacher@192.168.1.157>
2021-04-29 21:44:49 +0800

    Java

--- src/main/java/com/ocp/day18/School.java
+++ src/main/java/com/ocp/day18/School.java
@@ -3,6 +3,7 @@ package com.ocp.day18;
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class School {
     public static void main(String[] args) {
@@ -20,10 +21,11 @@ public class School {
         
         // 印出不及格的學生,分數,老師與總平均
         Consumer<Student> p = x -> System.out.printf("學生:%s, 分數:%d, 老師:%s\n", x.getName(), x.getScore(), x.getTeacher().getName());
+        Predicate<Student> notPass = x -> x.getScore() < 60;
         double avg = Arrays.stream(DataCenter.getPeople())
                 .filter(x -> x instanceof Student)
                 .map(x -> (Student)x)
-                .filter(x -> x.getScore() < 60)
+                .filter(notPass)
                 .peek(p)
                 .mapToInt(Student::getScore)
                 .average()
