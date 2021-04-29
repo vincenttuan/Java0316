e0585238ae373dfbde4df8885e270c8052d8caf0
MB-teacher <MB-teacher@192.168.1.157>
2021-04-29 19:52:51 +0800

    Java

---
+++ src/main/java/com/ocp/day17/LambdaDemo2.java
@@ -0,0 +1,11 @@
+package com.ocp.day17;
+
+import java.util.stream.Stream;
+
+public class LambdaDemo2 {
+    public static void main(String[] args) {
+        // 求此人總分與平均 = ?
+        Stream.of("國文", 100.0, "英文", 90.5, "數學", 70.5, "座號", 7);
+        
+    }
+}
