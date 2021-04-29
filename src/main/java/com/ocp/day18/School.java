19bd283fc0ebd0209460070a0b9b22a111d315a5
MB-teacher <MB-teacher@192.168.1.157>
2021-04-29 21:20:30 +0800

    Java

--- src/main/java/com/ocp/day18/School.java
+++ src/main/java/com/ocp/day18/School.java
@@ -1,12 +1,22 @@
 package com.ocp.day18;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 
 public class School {
     public static void main(String[] args) {
-        Arrays.stream(DataCenter.getPeople())
-                .forEach(System.out::println);
+        //Arrays.stream(DataCenter.getPeople())
+                //.peek(e -> System.out.println(e.getClass().getTypeName()))
+                //.forEach(System.out::println);
         // 請問考試的總分與平均 = ?
-        Arrays.stream(DataCenter.getPeople());
+        IntSummaryStatistics state = Arrays.stream(DataCenter.getPeople())
+                .filter(x -> x instanceof Student)
+                .map(x -> (Student)x)
+                .mapToInt(x -> x.getScore())
+                .summaryStatistics();
+        System.out.println(state.getSum());
+        System.out.println(state.getAverage());
+        // 請問老師薪資總和與平均 = ?
+        
     }
 }
