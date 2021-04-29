ad53ea03bf69ef088bf0a3f52b46708bca926bae
MB-teacher <MB-teacher@192.168.1.157>
2021-04-29 20:09:50 +0800

    Java

--- src/main/java/com/ocp/day17/LambdaDemo2.java
+++ src/main/java/com/ocp/day17/LambdaDemo2.java
@@ -8,7 +8,7 @@ public class LambdaDemo2 {
         // 求此人總分與平均 = ?
         DoubleSummaryStatistics stat = Stream.of("國文", 100.0, "英文", 90.5, "數學", 70.5, "座號", 7)
                 .filter(x -> x instanceof Double)
-                .mapToDouble(x -> (double)x) // 物件 Double 轉基本 double
+                .mapToDouble(x -> (double)x) // 物件 Double 轉基本 double 才會變成 DoubleStream
                 .summaryStatistics();
         
         System.out.println(stat.getSum());
