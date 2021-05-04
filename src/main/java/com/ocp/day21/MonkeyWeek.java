766851d124679a9e008ea77b699bdb58d042268f
MB-teacher <MB-teacher@192.168.1.157>
2021-05-04 20:47:57 +0800

    Java

--- src/main/java/com/ocp/day21/MonkeyWeek.java
+++ src/main/java/com/ocp/day21/MonkeyWeek.java
@@ -2,16 +2,14 @@ package com.ocp.day21;
 
 public class MonkeyWeek {
     public static void main(String[] args) {
-        work(2);
         work(Week.MON);
-        work(100); // 下錯參數
     }
-    public static void work(int num) {
-        switch(num) {
-            case Week.MON:
+    public static void work(Week week) {
+        switch(week) {
+            case MON:
                 System.out.println("猴子穿新衣");
                 break;
-            case Week.TUES:
+            case TUES:
                 System.out.println("猴子去烤肉");
                 break;
         }
--- src/main/java/com/ocp/day21/Week.java
+++ src/main/java/com/ocp/day21/Week.java
@@ -1,11 +1,5 @@
 package com.ocp.day21;
 
-public interface Week {
-    int SUN = 1;
-    int MON = 2;
-    int TUES = 3;
-    int WED = 4;
-    int THUR = 5;
-    int FRI = 6;
-    int SAT = 7;
+public enum Week {
+    SUN, MON, TUES, WED,THUR, FRI, SAT
 }
