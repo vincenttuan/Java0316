15ae3035e48965e3f3f67c34cb515fecc88f92fd
MB-teacher <MB-teacher@192.168.1.157>
2021-05-04 21:05:38 +0800

    Java

--- src/main/java/com/ocp/day21/USCurrencyDemo.java
+++ src/main/java/com/ocp/day21/USCurrencyDemo.java
@@ -3,7 +3,7 @@ package com.ocp.day21;
 public class USCurrencyDemo {
     public static void main(String[] args) {
         // 1, 5, 10, 25
-        USCurrency us = USCurrency.PENNY;
+        USCurrency us = USCurrency.DIME;
         System.out.println(us.getValue());
     }
 }
