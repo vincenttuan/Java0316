a3489a00c0b51f34c8acf0a4063b5927b07f5511
MB-teacher <MB-teacher@192.168.1.157>
2021-05-04 21:01:19 +0800

    Java

--- src/main/java/com/ocp/day21/USCurrency.java
+++ src/main/java/com/ocp/day21/USCurrency.java
@@ -1,11 +1,17 @@
 package com.ocp.day21;
 
-// PENNY 一美分, NICKLE 五美分, DIME 十美分, QUARTER 25美分
+// PENNY 一美分, NICKLE 五美分, 
+// DIME 十美分, QUARTER 25美分
 
-public class USCurrency {
+public enum USCurrency {
+    PENNY(1),
+    NICKLE(5),
+    DIME(10),
+    QUARTER(25);
+    
     private int value;
 
-    public USCurrency(int value) {
+    private USCurrency(int value) {
         this.value = value;
     }
     
---
+++ src/main/java/com/ocp/day21/USCurrencyDemo.java
@@ -0,0 +1,9 @@
+package com.ocp.day21;
+
+public class USCurrencyDemo {
+    public static void main(String[] args) {
+        // 1, 5, 10, 25
+        USCurrency us = USCurrency.PENNY;
+        System.out.println(us.getValue());
+    }
+}
