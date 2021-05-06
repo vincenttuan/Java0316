c9151bf4daff7fcf43d08cbd94641178b3b4f243
MB-teacher <MB-teacher@192.168.1.157>
2021-05-06 19:29:21 +0800

    Java

---
+++ src/main/java/com/ocp/day23/SetDemo1.java
@@ -0,0 +1,23 @@
+package com.ocp.day23;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class SetDemo1 {
+    public static void main(String[] args) {
+        Set subjects = new LinkedHashSet();
+        subjects.add("國"); // String
+        subjects.add("英"); // String
+        subjects.add(null); // null
+        subjects.add("數"); // String
+        subjects.add(100); // Integer
+        System.out.println(subjects);
+        // 走訪每一個元素
+        Iterator iter = subjects.iterator();
+        while (iter.hasNext()) {
+            Object next = iter.next();
+            System.out.println(next);
+        }
+    }
+}
