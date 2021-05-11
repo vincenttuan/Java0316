d022a963bdcb279a278a8a62a412c12fd4913a37
MB-teacher <MB-teacher@192.168.1.157>
2021-05-11 21:06:44 +0800

    Java

--- src/main/java/com/lab/order_system/ServiceCenter.java
+++ src/main/java/com/lab/order_system/ServiceCenter.java
@@ -13,7 +13,7 @@ public class ServiceCenter {
     }
     // 檢視 Catalog
     public void printCatalog() {
-        System.out.println("訂單系統");
+        System.out.println("訂單列表");
         System.out.println("+--------------------+");
         System.out.println("|No|　品名　|尺寸|Price|");
         System.out.println("+--------------------+");
@@ -22,5 +22,18 @@ public class ServiceCenter {
                     food.getNo(), food.getName(), food.getSize(), food.getPrice());
         }
         System.out.println("+--------------------+");
+    }
+    // 檢視系統 menu
+    public void menu() {
+        System.out.println("系統項目");
+        System.out.println("----------------------");
+        System.out.println("1. 檢視 Catalog");
+        System.out.println("2. 新增訂單餐點");
+        System.out.println("3. 查詢訂單餐點");
+        System.out.println("4. 取消訂單餐點");
+        System.out.println("5. 刪除全部訂單");
+        System.out.println("6. 結帳");
+        System.out.println("0. 離開");
+        System.out.println("----------------------");
     }
 }
