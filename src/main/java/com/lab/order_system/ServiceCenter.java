5bab8c05a85a22781529a6a2865aa541880c832b
MB-teacher <MB-teacher@192.168.1.157>
2021-05-11 19:57:37 +0800

    Java

--- src/main/java/com/lab/order_system/ServiceCenter.java
+++ src/main/java/com/lab/order_system/ServiceCenter.java
@@ -9,7 +9,7 @@ public class ServiceCenter {
         System.out.println("訂單系統");
         System.out.println("--------------------------------------------");
         for(Food food : catalog) {
-            System.out.printf("編號: %d 品名: %s 尺寸: %c 價格: %d",
+            System.out.printf("編號: %d 品名: %s 尺寸: %c 價格: %d\n",
                     food.getNo(), food.getName(), food.getSize(), food.getPrice());
         }
         System.out.println("--------------------------------------------");
--- src/test/java/test_order_system/TestServiceCenter.java
+++ src/test/java/test_order_system/TestServiceCenter.java
@@ -1,5 +1,12 @@
 package test_order_system;
 
+import com.lab.order_system.ServiceCenter;
+import org.junit.Test;
+
 public class TestServiceCenter {
-    
+    @Test
+    public void test_printCatalog() {
+        ServiceCenter sc = new ServiceCenter();
+        sc.printCatalog();
+    }
 }
