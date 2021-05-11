573a087b866d9dfc2f97374a7540e8ab8882caee
MB-teacher <MB-teacher@192.168.1.157>
2021-05-11 18:56:16 +0800

    Java

---
+++ src/main/java/com/ocp/day24/Food.java
@@ -0,0 +1,43 @@
+package com.ocp.day24;
+
+public class Food {
+    private String name;
+    private char size;
+    private int price;
+
+    public Food(String name, char size, int price) {
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSize() {
+        return size;
+    }
+
+    public void setSize(char size) {
+        this.size = size;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" + "name=" + name + ", size=" + size + ", price=" + price + '}';
+    }
+    
+}
