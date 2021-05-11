3dea44f61776c473a4533b39105cfe03776257bf
MB-teacher <MB-teacher@192.168.1.157>
2021-05-11 19:50:48 +0800

    Java

---
+++ src/main/java/com/lab/order_system/ServiceCenter.java
@@ -0,0 +1,17 @@
+package com.lab.order_system;
+
+import java.util.Set;
+
+public class ServiceCenter {
+    private Set<Food> catalog = new DataCenter().getCatalog();
+    // 檢視 Catalog
+    public void printCatalog() {
+        System.out.println("訂單系統");
+        System.out.println("--------------------------------------------");
+        for(Food food : catalog) {
+            System.out.printf("編號: %d 品名: %s 尺寸: %c 價格: %d",
+                    food.getNo(), food.getName(), food.getSize(), food.getPrice());
+        }
+        System.out.println("--------------------------------------------");
+    }
+}
