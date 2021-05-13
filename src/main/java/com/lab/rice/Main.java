fc7d45681bff3ab5b648ddb7a9f95b9c0c2edea8
MB-teacher <MB-teacher@192.168.1.157>
2021-05-13 21:22:47 +0800

    Java

--- src/main/java/com/lab/rice/Main.java
+++ src/main/java/com/lab/rice/Main.java
@@ -1,5 +1,7 @@
 package com.lab.rice;
 
+import java.util.List;
+import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
 
 public class Main {
@@ -7,5 +9,10 @@ public class Main {
         Stream.of(Util.getRices())
                 .filter(r -> r.get品名().contains("日本"))
                 .forEach(System.out::println);
+        // 將指定不合格的米放入集合中
+        List badRices = Stream.of(Util.getRices())
+                .filter(r -> r.get品名().contains("日本"))
+                .collect(toList()); // 將不合格的米收集到一個 list 集合中
+        System.out.printf("不合格筆數: %d, 內容: %s\n", badRices.size(), badRices);
     }
 }
