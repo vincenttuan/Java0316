a60f17eb88888e3d4b907a7675963393b5f1f60e
MB-teacher <MB-teacher@192.168.1.157>
2021-05-13 20:00:40 +0800

    Java

--- src/main/java/com/lab/cdc/Util.java
+++ src/main/java/com/lab/cdc/Util.java
@@ -1,14 +1,13 @@
 package com.lab.cdc;
 
 // 工具程式
-
 import com.google.gson.Gson;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Scanner;
 
 public class Util {
-    
+
     public static String getJson() throws Exception {
         // 資料來源
         String path = "https://www.cdc.gov.tw/TravelEpidemic/ExportJSON";
@@ -18,20 +17,44 @@ public class Util {
         String json = new Scanner(new InputStreamReader(url.openStream(), "UTF-8"))
                 .useDelimiter("\\A")
                 .next();
-        
+
 //        if(json.charAt(0) == '?') {
 //            return json.substring(1);
 //        } else {
 //            return json;
 //        }
-        
-        return (json.charAt(0) == '?')?json.substring(1):json;
+        return (json.charAt(0) == '?') ? json.substring(1) : json;
     }
-    
-    public static CDC[] getCdcs() throws Exception{
+
+    public static CDC[] getCdcs() throws Exception {
         String json = getJson();
         CDC[] cdcs = new Gson().fromJson(json, CDC[].class);
         return cdcs;
     }
-    
+
+    /**
+     * 计算地球上任意两点(经纬度)距离
+     *
+     * @param long1 第一点经度
+     * @param lat1 第一点纬度
+     * @param long2 第二点经度
+     * @param lat2 第二点纬度
+     * @return 返回距离 单位：米
+     */
+    public static double distance(double long1, double lat1, double long2,
+            double lat2) {
+        double a, b, R;
+        R = 6378137; // 地球半径
+        lat1 = lat1 * Math.PI / 180.0;
+        lat2 = lat2 * Math.PI / 180.0;
+        a = lat1 - lat2;
+        b = (long1 - long2) * Math.PI / 180.0;
+        double d;
+        double sa2, sb2;
+        sa2 = Math.sin(a / 2.0);
+        sb2 = Math.sin(b / 2.0);
+        d = 2 * R * Math.asin(Math.sqrt(sa2 * sa2 + Math.cos(lat1)
+                        * Math.cos(lat2) * sb2 * sb2));
+        return d;
+    }
 }
