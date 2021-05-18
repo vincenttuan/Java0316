672add150349f344dee64b635a5d9264c607d1c1
MB-teacher <MB-teacher@192.168.1.157>
2021-05-18 19:43:05 +0800

    Java

--- src/main/java/com/ocp/day25/VectorDemo.java
+++ src/main/java/com/ocp/day25/VectorDemo.java
@@ -4,11 +4,13 @@ import java.util.Vector;
 
 public class VectorDemo {
     public static void main(String[] args) {
-        Vector<Integer> v = new Vector<>();
+        Vector<Integer> v = new Vector<>(1300, 5);
         int n = 1300;
         for(int i=1;i<=n;i++) {
             v.add(i);
         }
+        v.add(10);
+        v.add(20);
         System.out.printf("capacity(容積空間): %d\n", v.capacity());
         System.out.printf("size(已使用): %d\n", v.size());
         System.out.printf("殘餘空間: %d\n", v.capacity() - v.size());
