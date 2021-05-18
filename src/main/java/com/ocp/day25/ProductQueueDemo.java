d7f58a545582c85627b7a80a346dcfec9b922bba
MB-teacher <MB-teacher@192.168.1.157>
2021-05-18 19:19:30 +0800

    Java

---
+++ src/main/java/com/ocp/day25/ProductQueueDemo.java
@@ -0,0 +1,26 @@
+package com.ocp.day25;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ProductQueueDemo {
+    public static void main(String[] args) {
+        Product p1 = new Product("口罩", 20);
+        Product p2 = new Product("酒精", 50);
+        Product p3 = new Product("手套", 15);
+        
+        Queue<Product> products = new LinkedList<>();
+        products.offer(p1);
+        products.offer(p1);
+        products.offer(p2);
+        products.offer(p2);
+        products.offer(p2);
+        products.offer(p3);
+        System.out.println("購物車: " + products);
+        // 結帳
+        
+        // 應付帳款
+        
+        // 查看購物車
+    }
+}
