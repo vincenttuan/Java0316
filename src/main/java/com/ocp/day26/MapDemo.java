ff5c9c10cbe6abf60dd2a0bf8865021b470a4b85
MB-teacher <MB-teacher@192.168.1.157>
2021-05-18 20:52:16 +0800

    Java

--- src/main/java/com/ocp/day26/MapDemo.java
+++ src/main/java/com/ocp/day26/MapDemo.java
@@ -1,8 +1,10 @@
 package com.ocp.day26;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -25,5 +27,7 @@ public class MapDemo {
         // 取得所有考試成績
         Collection<Integer> scores = exams.values();
         System.out.println(scores);
+        List<Integer> scores2 = new ArrayList<>(scores);
+        System.out.println(scores2);
     }
 }
