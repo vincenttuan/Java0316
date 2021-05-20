496c69b743fe8654cbdcce4eeab30fb3e2619991
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-20 20:46:08 +0800

    Java

---
+++ src/main/java/com/ocp/day27/GroupingExam.java
@@ -0,0 +1,19 @@
+package com.ocp.day27;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupingExam {
+    public static void main(String[] args) {
+        List<Exam> exams = Arrays.asList(
+                new Exam("John", 100),
+                new Exam("Mary", 50),
+                new Exam("Helen", 70),
+                new Exam("Tom", 80),
+                new Exam("Bob", 30)
+        );
+        // 請問及格/不及格的人數分組
+        // {true = 3, false = 2}
+        
+    }
+}
