8297183058977225b0a8b5b62f404183d742f66f
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-20 21:46:16 +0800

    Java

--- src/main/java/com/ocp/day28/ExceptionDemo1.java
+++ src/main/java/com/ocp/day28/ExceptionDemo1.java
@@ -1,13 +1,26 @@
 package com.ocp.day28;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionDemo1 {
+
     public static void main(String[] args) {
+        play();
+    }
+
+    public static void play() {
         System.out.println("請輸入半徑: ");
         Scanner scanner = new Scanner(System.in);
-        double r = scanner.nextDouble();
-        double area = Math.pow(r, 2) * Math.PI;
-        System.out.printf("Area: %.2f\n", area);
+
+        try {
+            double r = scanner.nextDouble();
+            double area = Math.pow(r, 2) * Math.PI;
+            System.out.printf("Area: %.2f\n", area);
+        } catch (InputMismatchException e) {
+            System.out.println("錯誤被捉到了, 內容: " + e);
+            System.out.println("半徑請輸入數字");
+        }
+
     }
 }
