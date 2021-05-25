fb35ecc92a3f2fc0e88fe5eec856651e7ebaa5b3
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-25 19:36:45 +0800

    Java

---
+++ src/main/java/com/ocp/day28/ExceptionDemo7.java
@@ -0,0 +1,20 @@
+package com.ocp.day28;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExceptionDemo7 {
+    public static void main(String[] args) {
+        String mybirthday = "2000/1/一";
+        DateFormat df = DateFormat.getDateInstance();
+        try {
+            Date date = df.parse(mybirthday);
+            System.out.println(date);
+        } catch (ParseException e) {
+            System.out.println("日期轉換失敗, " + e);
+        }
+    }
+}
