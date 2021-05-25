d2f93c6940ec444dbe9cc3ff093dfd8c8dff7b4c
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-25 22:08:00 +0800

    Java

--- src/main/java/com/ocp/day28/LoginCheckMain.java
+++ src/main/java/com/ocp/day28/LoginCheckMain.java
@@ -3,7 +3,17 @@ package com.ocp.day28;
 public class LoginCheckMain {
     public static void main(String[] args) {
         
+        String name = "admin";
+        String password = "1234";
         
+        LoginService service = new LoginService();
+        
+        try {
+            System.out.println(service.login(name, password));
+        } catch (LoginFailException e) {
+            System.out.println(e.getMessage());
+            e.怎麼辦();
+        }
         
     }
 }
