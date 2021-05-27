73eb468ccacf055e1af27125c4e1523ebc340233
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-27 19:38:27 +0800

    Java

---
+++ src/main/java/com/ocp/day29_assert/Power.java
@@ -0,0 +1,16 @@
+package com.ocp.day29_assert;
+
+// 電力換算
+// W(瓦數), V(電壓), A(電流)
+// w = v * a
+public class Power {
+    // 取得 W , 方法 getW(int v, int a)
+    
+    
+    // 取得 A, 方法 getA(int w, int v)
+    
+    
+    // 取得 V, 方法 getV(int w, int a)
+    
+    
+}
---
+++ src/test/java/test_power/Test_Power.java
@@ -0,0 +1,20 @@
+package test_power;
+
+import org.junit.Test;
+
+public class Test_Power {
+    @Test
+    public void t1() {
+    
+    }
+    
+    @Test
+    public void t2() {
+    
+    }
+    
+    @Test
+    public void t3() {
+    
+    }
+}
