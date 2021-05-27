97b55951d790be9a10ba57c9baf133667595dd94
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-27 20:49:52 +0800

    Java

--- src/main/java/com/ocp/day29_assert/FinalizeDemo.java
+++ src/main/java/com/ocp/day29_assert/FinalizeDemo.java
@@ -1,24 +1,33 @@
 package com.ocp.day29_assert;
 
+// main -> 主執行緒, Finalizer -> 終結者執行緒
 class Ball {
     // 建構子
     public Ball() {
-        System.out.println("Ball 建立-棒球");
+        String tname = Thread.currentThread().getName();
+        System.out.println("Ball 建立-棒球 : " + tname);
     }
     
     // 商業邏輯
     void play() {
-        System.out.println("打棒球");
+        String tname = Thread.currentThread().getName();
+        System.out.println("打棒球 : " + tname);
     }
     
     // Finalize
     @Override
     protected void finalize() throws Throwable {
-        System.out.println("Ball 消失了");
+        String tname = Thread.currentThread().getName();
+        System.out.println("Ball 消失了 : " + tname);
     }
 
 }
 
 public class FinalizeDemo {
-    
+    public static void main(String[] args) {
+        Ball b = new Ball();
+        b.play();
+        b = null;
+        System.gc();
+    }
 }
