44e9dbcc71e00ea9402dd9b4af80e3f449fae8c8
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-01 20:02:35 +0800

    Java

---
+++ src/main/java/com/ocp/day30_thread/CallableDemo1.java
@@ -0,0 +1,30 @@
+package com.ocp.day30_thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+// 匯率
+class Exchange implements Callable<Double> {
+    @Override
+    public Double call() throws Exception {
+        return 27.81;
+    }
+}
+
+public class CallableDemo1 {
+    public static void main(String[] args) throws Exception {
+        double usd = 1000;
+        // 換匯
+        Exchange e = new Exchange();
+        FutureTask<Double> task = new FutureTask<>(e);
+        new Thread(task).start();
+        // 取得匯率結果
+        double rate = task.get();
+        // 計算
+        int ntd = (int)(usd * rate);
+        System.out.printf("美金 $%.1f 可換 台幣 $%,d 匯率: %.2f\n", 
+                           usd, ntd, rate);
+        
+    }
+}
