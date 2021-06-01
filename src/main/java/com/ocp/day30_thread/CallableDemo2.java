c6b94d9d2466b3a1946912be8b5f69109a0b7600
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-01 21:08:48 +0800

    Java

--- src/main/java/com/ocp/day30_thread/CallableDemo2.java
+++ src/main/java/com/ocp/day30_thread/CallableDemo2.java
@@ -2,6 +2,7 @@ package com.ocp.day30_thread;
 
 import java.math.BigDecimal;
 import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
@@ -27,12 +28,23 @@ class MyStock implements Callable<Double> {
 }
 
 public class CallableDemo2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 假設欲購買台積電 1500 股請問交易成本為何 ?
         // 手續費 = 交易成本 * 0.001425
-        String symbol = "2330.TW";
+        String symbol = "3008.TW"; // "2330.TW";
         int amount = 1500; // 股
         
+        MyStock myStock = new MyStock(symbol);
+        FutureTask<Double> task = new FutureTask<>(myStock);
+        new Thread(task).start();
+        
+        double price = task.get();
+        double cost  = price * amount;
+        double fee = cost * 0.001425;
+        double total = cost + fee;
         // 印出交易成本（含手續費）
+        
+        System.out.printf("買進 %s price: $%,.1f amount: %,d 股 cost: $%,.1f fee: $%,.1f total: $%,.1f\n",
+                symbol, price, amount, cost, fee, total);
     }
 }
