5c751e5db31a338ef28ad406193aea882441c9df
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-01 21:41:29 +0800

    Java

--- src/main/java/com/ocp/day30_thread/ATM.java
+++ src/main/java/com/ocp/day30_thread/ATM.java
@@ -52,6 +52,10 @@ public class ATM {
     public static void main(String[] args) {
         Account account = new Account(10000);
         Thread t1 = new Thread(new WithDraw(account, 5000), "小明");
+        Thread t2 = new Thread(new WithDraw(account, 3000), "小華");
+        Thread t3 = new Thread(new WithDraw(account, 4000), "小英");
         t1.start();
+        t2.start();
+        t3.start();
     }
 }
