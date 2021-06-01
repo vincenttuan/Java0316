b4933c361c2ea9d92e2bb6b7d180340ca84fe424
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-01 20:42:55 +0800

    Java

---
+++ src/main/java/com/ocp/day30_thread/CallableDemo2.java
@@ -0,0 +1,15 @@
+package com.ocp.day30_thread;
+/*
+利用 Callable 與 YahooFinance api 取得台積電最新報價
+台積電的代號: 2330.TW
+*/
+
+public class CallableDemo2 {
+    public static void main(String[] args) {
+        // 假設欲購買台積電 1500 股請問交易成本為何 ?
+        // 手續費 = 交易成本 * 0.001425
+        int amount = 1500; // 股
+        
+        // 印出交易成本（含手續費）
+    }
+}
