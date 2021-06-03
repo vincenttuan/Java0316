445127d74641649edbb3126110eacd1bcb32872b
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-03 19:00:47 +0800

    Java

--- src/main/java/com/ocp/day31_thread/GetBall.java
+++ src/main/java/com/ocp/day31_thread/GetBall.java
@@ -1,9 +1,9 @@
 package com.ocp.day31_thread;
 
 class Ball {
-    private int max = 10;
-    public void get() {
-        while(max > 0) {
+    private int max = 100;
+    public synchronized void get() {
+        if(max > 0) {
             String name = Thread.currentThread().getName();
             System.out.printf("%s 取到第 %d 顆球\n", name, max);
             max--;
@@ -20,13 +20,16 @@ class BallThread extends Thread {
 
     @Override
     public void run() {
-        ball.get();
+        for(int i=1;i<=100;i++) {
+            ball.get();
+        }
     }
     
 }
 public class GetBall {
     public static void main(String[] args) {
         Ball ball = new Ball();
+        new BallThread(ball).start();
         new BallThread(ball).start();
     }
 }
