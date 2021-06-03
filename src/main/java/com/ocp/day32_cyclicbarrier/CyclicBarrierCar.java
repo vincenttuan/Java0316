c1dd4f625898b30080f0cfa0679eca42f9f5d477
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-03 20:51:38 +0800

    Java

--- src/main/java/com/ocp/day32_cyclicbarrier/CyclicBarrierCar.java
+++ src/main/java/com/ocp/day32_cyclicbarrier/CyclicBarrierCar.java
@@ -5,7 +5,9 @@ import java.util.concurrent.CyclicBarrier;
 public class CyclicBarrierCar {
     public static void main(String[] args) {
         int parties = 3;
-        CyclicBarrier cb = new CyclicBarrier(parties);
+        //CyclicBarrier cb = new CyclicBarrier(parties);
+        Runnable r = () -> System.out.println("一起吃午餐");
+        CyclicBarrier cb = new CyclicBarrier(parties, r);
         new Car(cb).start();
         new Car(cb).start();
         new Car(cb).start();
