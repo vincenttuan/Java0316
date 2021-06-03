784ad3be7e3b70d42cf66b57b2f76046dc7e7a47
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-03 19:11:51 +0800

    Java

--- src/main/java/com/ocp/day31_thread/GetBall.java
+++ src/main/java/com/ocp/day31_thread/GetBall.java
@@ -1,5 +1,8 @@
 package com.ocp.day31_thread;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 class Ball {
     private int max = 500;
     public void get() {
@@ -9,6 +12,7 @@ class Ball {
                 String name = Thread.currentThread().getName();
                 System.out.printf("%s 取到第 %d 顆球\n", name, max);
                 max--;
+                GetBall.map.put(name, GetBall.map.get(name)+1);
             }
         }
         // ... code
@@ -27,10 +31,17 @@ class BallThread extends Thread {
         for(int i=1;i<=500;i++) {
             ball.get();
         }
+        System.out.println(GetBall.map);
     }
     
 }
 public class GetBall {
+    static Map<String, Integer> map = new LinkedHashMap<>();
+    static {
+        map.put("Thread-0", 0);
+        map.put("Thread-1", 0);
+        map.put("Thread-2", 0);
+    }
     public static void main(String[] args) {
         Ball ball = new Ball();
         new BallThread(ball).start();
