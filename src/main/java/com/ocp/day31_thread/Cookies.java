aa8a408ed9ed071bdf4dc3cde5c50222207c1791
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-03 20:01:42 +0800

    Java

--- src/main/java/com/ocp/day31_thread/Cookies.java
+++ src/main/java/com/ocp/day31_thread/Cookies.java
@@ -3,7 +3,7 @@ package com.ocp.day31_thread;
 public class Cookies {
     private boolean empty = true; // 盤子
     public synchronized void eat(int i) {
-        if(empty == true) { // 盤子是空的
+        while(empty == true) { // 盤子是空的
             try {
                wait(); 
             } catch (InterruptedException e) {
@@ -14,10 +14,10 @@ public class Cookies {
         System.out.printf("小狗吃了第 %d 塊餅乾\n", i);
         // 吃完之後
         empty = true; // 盤子變空
-        notify(); // 呼叫主人
+        notifyAll(); // 呼叫主人
     }
     public synchronized void put(int i) {
-        if(empty == false) { // 盤子不是空的
+        while(empty == false) { // 盤子不是空的
             try {
                wait(); 
             } catch (InterruptedException e) {
@@ -28,6 +28,6 @@ public class Cookies {
         System.out.printf("主人放了第 %d 塊餅乾\n", i);
         // 主人放完之後
         empty = false; // 盤子非空
-        notify(); // 呼叫小狗
+        notifyAll(); // 呼叫小狗
     }
 }
