7372ce95ac24f5fec816bc07fb88d48929dda76b
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-08 18:55:47 +0800

    JavaWeb

--- src/main/java/com/ocp/day34/TestTask.java
+++ src/main/java/com/ocp/day34/TestTask.java
@@ -1,15 +1,19 @@
 package com.ocp.day34;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 class Task implements Runnable { // 短任務
+
     @Override
     public void run() {
         System.out.println("短任務完成");
     }
 }
 
-class LongTest implements Runnable { // 長任務
+class LongTast implements Runnable { // 長任務
+
     @Override
     public void run() {
         System.out.println("長任務開始");
@@ -20,6 +24,22 @@ class LongTest implements Runnable { // 長任務
         System.out.println("長任務結束");
     }
 }
+
 public class TestTask {
-    
+
+    public static void main(String[] args) throws Exception {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(4);
+        service.submit(new Task());
+        service.submit(new Task());
+        service.submit(new LongTast());
+        service.submit(new Task());
+
+        service.shutdown();
+
+        // 偵測 pool 有沒有關閉
+        while (!service.awaitTermination(1, TimeUnit.SECONDS)) {
+            System.out.println("Pool not close ...");
+        }
+        System.out.println("Pool closed");
+    }
 }
