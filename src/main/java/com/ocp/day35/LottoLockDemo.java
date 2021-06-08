b7a3f9f4a06306b105fcd31d923cae84329c26cb
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-08 21:40:01 +0800

    JavaWeb

--- src/main/java/com/ocp/day35/LottoLockDemo.java
+++ src/main/java/com/ocp/day35/LottoLockDemo.java
@@ -1,13 +1,16 @@
 package com.ocp.day35;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
 class LottoLock implements Runnable {
-    //private static final ReentrantLock LOCK = new ReentrantLock();
+    private static final ReentrantLock LOCK = new ReentrantLock();
     @Override
     public void run() {
         try {
+            LOCK.lock(); // 嘗試鎖定
             // 取得五組 Lotto 號碼
             Random r = new Random();
             for (int i = 1; i <= 5; i++) {
@@ -17,6 +20,8 @@ class LottoLock implements Runnable {
                         Thread.currentThread().getName(), i, n);
             }
         } catch (Exception e) {
+        } finally {
+            LOCK.unlock(); // 解鎖
         }
 
     }
@@ -24,5 +29,10 @@ class LottoLock implements Runnable {
 }
 
 public class LottoLockDemo {
-
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.execute(new LottoLock());
+        service.execute(new LottoLock());
+        service.shutdown();
+    }
 }
