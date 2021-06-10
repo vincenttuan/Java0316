ec76780eacf3f344a016ff6e8104f11dd6cfd9a2
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-10 19:19:10 +0800

    JavaWeb

--- src/main/java/com/ocp/day36_io/ReadFile_Salary.java
+++ src/main/java/com/ocp/day36_io/ReadFile_Salary.java
@@ -6,17 +6,23 @@ import java.io.FileReader;
 public class ReadFile_Salary {
     public static void main(String[] args) {
         File file = new File("src/main/java/com/ocp/day36_io/files/salary.txt");
+        FileReader fr = null;
         try {
-            FileReader fr = new FileReader(file);
+            fr = new FileReader(file); // 開啟檔案
             int ch = 0;
 //            System.out.println(fr.read());
 //            System.out.println(fr.read());
 //            System.out.println(fr.read());
 //            System.out.println(fr.read());
-            while ((ch = fr.read()) != -1) {                
+            while ((ch = fr.read()) != -1) { // 讀取內容             
                 System.out.print((char)ch);
             }
         } catch (Exception e) {
+        } finally {
+            try {
+                fr.close(); // 關閉檔案
+            } catch (Exception e) {
+            }
         }
         
     }
