f634eb74d88af71d23ba6ec85eb683c54221253d
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-10 18:57:59 +0800

    JavaWeb

--- src/main/java/com/ocp/day36_io/CreateFolderAndFile.java
+++ src/main/java/com/ocp/day36_io/CreateFolderAndFile.java
@@ -4,13 +4,18 @@ import java.io.File;
 
 
 public class CreateFolderAndFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 想在 day36_io 資料夾下面 建立 files 的資料夾
         File folder = new File("src/main/java/com/ocp/day36_io/files");
         if(!folder.exists()) {
             folder.mkdir(); // 建立資料夾
         }
         // 然後 day36_io/files 資料夾下面 建立 salary.txt 的檔案
-        
+        if(folder.exists()) {
+            File f = new File("src/main/java/com/ocp/day36_io/files/salary.txt");
+            if(!f.exists()) {
+                f.createNewFile();
+            }
+        }
     }
 }
